3dd0e6892af02b2355c4119539ac79b2b1084387
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-28 22:56:35 +0200

    added ButtonExample

---
+++ src/components/neobrutalism-examples/ButtonExample.tsx
@@ -0,0 +1,7 @@
+export default function ButtonExample() {
+  return (
+    <button className="flex cursor-pointer items-center rounded-md border-2 border-black bg-[#bc95d4] px-10 py-3 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none">
+      Button
+    </button>
+  )
+}
