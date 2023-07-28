68adef180dab834f2e46ce1c9fbd464fc7202ae7
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-28 23:19:57 +0200

    added InputExample

---
+++ src/components/neobrutalism-examples/InputExample.tsx
@@ -0,0 +1,11 @@
+export default function InputExample() {
+  return (
+    <input
+      className="rounded-md border-2 border-black p-[10px] font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] outline-none transition-all focus:translate-x-[3px] focus:translate-y-[3px] focus:shadow-none"
+      type="text"
+      name="text"
+      id="text"
+      placeholder="Placeholder"
+    />
+  )
+}
