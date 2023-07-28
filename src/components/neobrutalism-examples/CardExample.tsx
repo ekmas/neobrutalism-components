62d35a19e9866dd1d234495fa40e21389bddaa10
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-28 23:07:04 +0200

    added CardExample

---
+++ src/components/neobrutalism-examples/CardExample.tsx
@@ -0,0 +1,15 @@
+export default function CardExample() {
+  return (
+    <div className="w-[250px] rounded-md border-2 border-black bg-[#bc95d4] font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
+      <div className="border-b-2 border-black p-4">
+        <h2 className="text-lg">Heading</h2>
+      </div>
+      <div className="p-4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          maiores delectus totam repellat provident minima
+        </p>
+      </div>
+    </div>
+  )
+}
