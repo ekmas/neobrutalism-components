a157cd1d9a2e279f775c6991bec79b40255796d7
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-28 22:38:40 +0200

    fixed Alert icon and added AlertExample

--- src/components/neobrutalism-examples/AlertExample.tsx
+++ src/components/neobrutalism-examples/AlertExample.tsx
@@ -1,10 +1,11 @@
 import { TbAlertOctagonFilled } from 'react-icons/tb'
 
-export default function Alert({ message }: { message: string }) {
+export default function AlertExample() {
   return (
     <div className="flex items-center justify-center rounded-md border-2 border-black bg-[#bc95d4] p-5 px-8 font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
       <TbAlertOctagonFilled className="mr-3 h-6 min-h-[24px] w-6 min-w-[24px]" />
-      {message}
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor
+      sit amet consectetur adipisicing elit.
     </div>
   )
 }
