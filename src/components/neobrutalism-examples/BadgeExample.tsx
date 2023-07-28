6d4435c33984398d9c6ac2de2801736c51a379c3
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-28 22:53:12 +0200

    added BadgeExample

---
+++ src/components/neobrutalism-examples/BadgeExample.tsx
@@ -0,0 +1,7 @@
+export default function BadgeExample() {
+  return (
+    <div className="w-min rounded-full border-2 border-black bg-[#bc95d4] px-3 py-1.5 text-sm font-bold shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none">
+      Badge
+    </div>
+  )
+}
