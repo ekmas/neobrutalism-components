5ded2de979017e0f58bf93a11f677cc92ee3f0e4
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-30 02:56:20 +0200

    added DocsMain

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,9 +1,11 @@
+import DocsMain from '@/components/app/DocsMain'
 import Sidebar from '@/components/app/Sidebar'
 
 export default function Docs() {
   return (
     <>
       <Sidebar />
+      <DocsMain />
     </>
   )
 }
---
+++ src/components/app/DocsMain.tsx
@@ -0,0 +1,7 @@
+export default function DocsMain() {
+  return (
+    <div className="docs ml-[250px] w-[full-250px] bg-[#c9dcd8] px-5 pt-[80px]">
+      <div className="mx-auto w-[600px] py-16"></div>
+    </div>
+  )
+}
