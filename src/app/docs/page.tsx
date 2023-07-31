53e4fb61f3b12e66c913e3bf023a3fa6f4f8094f
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-07-31 07:18:47 +0200

    added metadata

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,5 +1,13 @@
 import DocsMain from '@/components/app/DocsMain'
 import Sidebar from '@/components/app/Sidebar'
+import { Metadata } from 'next'
+import { metadata as rootMetadata } from '../layout'
+
+export const metadata: Metadata = {
+  ...rootMetadata,
+  title: 'Documentation - Neobrtualism Components',
+  description: 'Documentation for neobrutalism components.',
+}
 
 export default function Docs() {
   return (
