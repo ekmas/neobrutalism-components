ab334d086a2e675e849a8da5b73c70e45c933c29
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-10-08 03:47:48 +0200

    updated installation guide

--- src/components/app/Installation.tsx
+++ src/components/app/Installation.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Installation() {
   return (
     <>
-      <h2 className="mt-5 text-2xl font-bold m400:text-xl">Installation</h2>
+      <h2 className="mt-5 text-2xl font-bold m400:text-xl">
+        Installation & usage
+      </h2>
 
       <div className="mt-8 font-bold leading-relaxed">
         These React components use
@@ -24,8 +26,22 @@ export default function Installation() {
         </a>
         for icons, so make sure you got them installed.
         <br />
+        <br />
         Now choose any component you find useful, copy it to your project and
         adjust it so it fulfills your needs.
+        <br />
+        <br />
+        Keep in mind that these are YOUR components. It&apos;s on you to make
+        them more reusable and accessible. I created this to help you get
+        started with neobrutalism.
+        <a
+          href="https://www.youtube.com/watch?v=eXRlVpw1SIQ&ab_channel=Joshtriedcoding"
+          target="_blank"
+          className="mx-1 underline"
+        >
+          This video
+        </a>{' '}
+        can help you with creating more reusable components.
       </div>
     </>
   )
