0db95f50f36eec4d295654db2d56e70a4f6f8c30
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-02-02 13:45:29 +0100

    added colors and screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        bg: '#DAF5F0',
+        main: '#C4A1FF',
+      },
       animation: {
         marquee: 'marquee 5s linear infinite',
         marquee2: 'marquee2 5s linear infinite',
@@ -22,6 +26,9 @@ module.exports = {
         },
       },
       screens: {
+        m1500: { raw: '(max-width: 1500px)' },
+        m1300: { raw: '(max-width: 1300px)' },
+        m1100: { raw: '(max-width: 1100px)' },
         m1000: { raw: '(max-width: 1000px)' },
         m900: { raw: '(max-width: 900px)' },
         m850: { raw: '(max-width: 850px)' },
