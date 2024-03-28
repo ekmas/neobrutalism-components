eb19f66a2388185421c40d3a334574b4d0761d50
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-28 09:04:29 +0100

    added highlight.js

--- src/components/app/Code.tsx
+++ src/components/app/Code.tsx
@@ -1,11 +1,22 @@
+import hljs from 'highlight.js/lib/core'
+import moonscript from 'highlight.js/lib/languages/moonscript'
+import 'highlight.js/styles/ascetic.css'
+
+hljs.registerLanguage('moonscript', moonscript)
+// there's no tsx, and typescript doesn't highlight markup so this is the workaround
+
 const Code = async ({ code }: { code: string }) => {
-  const component = code
+  const component = hljs.highlight(code, { language: 'moonscript' }).value
 
   return (
-    <div
-      className="code w-max-[700px] scrollbar mt-5 h-[300px] overflow-x-auto rounded-md border-2 border-black bg-[#151515] p-5 text-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] m1000:w-[500px] m750:h-[180px] m750:w-[350px] m400:w-full m400:text-xs"
-      dangerouslySetInnerHTML={{ __html: component }}
-    />
+    <pre>
+      <code>
+        <div
+          className="code w-max-[700px] scrollbar mt-5 h-[300px] overflow-x-auto rounded-md border-2 border-black bg-white p-5 text-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] m1000:w-[500px] m750:h-[180px] m750:w-[350px] m400:w-full m400:text-xs"
+          dangerouslySetInnerHTML={{ __html: component }}
+        />
+      </code>
+    </pre>
   )
 }
 
