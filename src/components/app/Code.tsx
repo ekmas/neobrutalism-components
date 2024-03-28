df4a264713b3aeacba493b2220ce8e9f2c26b6c9
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-03-28 10:35:00 +0100

    added name and limitedHeight to Code component

--- src/components/app/Code.tsx
+++ src/components/app/Code.tsx
@@ -5,18 +5,32 @@ import 'highlight.js/styles/ascetic.css'
 hljs.registerLanguage('moonscript', moonscript)
 // there's no tsx, and typescript doesn't highlight markup so this is the workaround
 
-const Code = async ({ code }: { code: string }) => {
+const Code = async ({
+  code,
+  name,
+  limitedHeight = true,
+}: {
+  code: string
+  name: string
+  limitedHeight?: boolean
+}) => {
   const component = hljs.highlight(code, { language: 'moonscript' }).value
 
   return (
-    <pre>
-      <code>
-        <div
-          className="code w-max-[700px] scrollbar mt-5 h-[300px] overflow-x-auto rounded-md border-2 border-black bg-white p-5 text-sm shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] m1000:w-[500px] m750:h-[180px] m750:w-[350px] m400:w-full m400:text-xs"
-          dangerouslySetInnerHTML={{ __html: component }}
-        />
-      </code>
-    </pre>
+    <div className="mt-5 rounded-md border-2 border-black shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]">
+      <div className="rounded-t-md border-b-2 border-black bg-white p-1 px-3">
+        {name}
+      </div>
+      <pre>
+        <code>
+          <div
+            style={{ height: limitedHeight ? '300px' : 'unset' }}
+            className="code w-max-[700px] scrollbar overflow-x-auto bg-white p-5 text-sm  m1000:w-[500px] m750:h-[180px] m750:w-[350px] m400:w-full m400:text-xs"
+            dangerouslySetInnerHTML={{ __html: component }}
+          />
+        </code>
+      </pre>
+    </div>
   )
 }
 
