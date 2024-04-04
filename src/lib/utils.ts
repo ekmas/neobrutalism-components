f531abd930ee0222df84d9b472c35f10481f5484
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-04-04 22:29:20 +0200

    added utility functions

---
+++ src/lib/utils.ts
@@ -0,0 +1,18 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function addSpaces(name: string) {
+  return name.replace(/([a-z])([A-Z])/g, '$1 $2');
+}
+
+export function transformToSlug(input: string): string {
+  return input.toLowerCase().replace(/\s+/g, '-');
+}
+
+export function transformToName(input: string): string {
+  return input.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+}
