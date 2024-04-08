a379cef2bf9cf7012068b6140245191e51164d74
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-04-08 20:36:56 +0200

    updated Sidebar component

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -2,34 +2,23 @@ import { Metadata } from 'next'
 
 import { MAIN_SIDEBAR } from '@/data/sidebar-links'
 
-import Sidebar, { SidebarHeader, SidebarLink } from '@/components/app/Sidebar'
+import DocsLayout from '@/layouts/Docs'
+import Sidebar from '@/components/app/Sidebar'
 
 export const metadata: Metadata = {
   title: 'Documentation - Neobrutalism Components',
   description: 'Documentation for neobrutalism components.',
 }
 
-export default function DocsLayout({
+export default function MainDocsLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
     <>
-      <Sidebar>
-        {MAIN_SIDEBAR.map((item, id) => {
-          return typeof item === 'string' ? (
-            <SidebarHeader text={item} key={id} />
-          ) : (
-            <SidebarLink href={item.href} text={item.text} key={id} />
-          )
-        })}
-      </Sidebar>
-      <div className="docs ml-[250px] min-h-[100dvh] w-[full-250px] bg-bg px-5 pt-[88px] m800:ml-[180px] m800:w-[full-180px] m600:m-0 m600:w-full m500:pt-16">
-        <div className="mx-auto w-[700px] py-20 font-bold leading-relaxed prose-h2:mb-8 prose-h2:text-2xl prose-h3:mb-4 prose-h3:text-xl prose-a:underline m1000:w-[500px] m750:w-[350px] m400:w-full m400:py-16 prose-h2:m400:text-xl prose-h3:m400:text-lg">
-          {children}
-        </div>
-      </div>
+      <Sidebar links={MAIN_SIDEBAR} />
+      <DocsLayout>{children}</DocsLayout>
     </>
   )
 }
--- src/components/app/Sidebar.tsx
+++ src/components/app/Sidebar.tsx
@@ -1,28 +1,36 @@
 import Link from 'next/link'
 
-export default function Sidebar({ children }: { children: React.ReactNode }) {
+export default function Sidebar({
+  links,
+}: {
+  links: (
+    | string
+    | {
+        href: string
+        text: string
+      }
+  )[]
+}) {
   return (
     <aside className="scrollbar fixed top-[88px] h-[calc(100svh-88px)] max-h-[calc(100svh-88px)] w-[250px] overflow-y-auto border-r-4 border-black m800:w-[180px] m600:hidden">
-      {children}
+      {links.map((item, id) => {
+        return typeof item === 'string' ? (
+          <div
+            key={id}
+            className="sidebaritem block border-b-4 border-r-4 border-black p-4 text-xl font-bold m800:p-4 m800:text-base"
+          >
+            {item}
+          </div>
+        ) : (
+          <Link
+            key={id}
+            href={`${item.href}`}
+            className="sidebaritem block border-b-4 border-r-4 border-black p-4 pl-7 text-lg font-semibold text-black/90 hover:bg-main m800:p-4 m800:pl-6 m800:text-base"
+          >
+            {item.text}
+          </Link>
+        )
+      })}
     </aside>
-  )
-}
-
-export function SidebarLink({ href, text }: { href: string; text: string }) {
-  return (
-    <Link
-      href={`${href}`}
-      className="sidebaritem block border-b-4 border-r-4 border-black p-4 pl-7 text-lg font-semibold text-black/90 hover:bg-main m800:p-4 m800:pl-6 m800:text-base"
-    >
-      {text}
-    </Link>
-  )
-}
-
-export function SidebarHeader({ text }: { text: string }) {
-  return (
-    <div className="sidebaritem block border-b-4 border-r-4 border-black p-4 text-xl font-bold m800:p-4 m800:text-base">
-      {text}
-    </div>
   )
 }
