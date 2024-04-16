5a852af914aa959d5569b162e2fdada63a52a4a3
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-04-16 17:02:48 +0200

    added markdown dir to tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/markdown/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
