cc6b01064f0930a8acb32cb746159e724e7cc619
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-10-20 04:46:36 +0200

    added some mdx components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,7 +1,51 @@
 import type { MDXComponents } from 'mdx/types'
 
+import Link from 'next/link'
+
+import EditThisPage from '@/components/app/EditThisPage'
+import Pagination from '@/components/app/Pagination'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+import { cn } from '@/lib/utils'
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    Tabs: ({ className, ...props }: React.ComponentProps<typeof Tabs>) => (
+      <Tabs className={cn(className, 'w-full')} {...props} />
+    ),
+    TabsList: ({
+      className,
+      ...props
+    }: React.ComponentProps<typeof TabsList>) => (
+      <TabsList
+        className={cn(
+          className,
+          'w-full overflow-x-hidden rounded-b-none m750:h-10 shadow-light rounded-t-base bg-white p-0 dark:bg-secondaryBlack',
+        )}
+        {...props}
+      />
+    ),
+    TabsTrigger: ({
+      className,
+      ...props
+    }: React.ComponentProps<typeof TabsTrigger>) => (
+      <TabsTrigger
+        className={cn(
+          className,
+          'h-full border-0 border-r-2 z-10 m400:text-xs border-r-border rounded-none sm:text-base bg-main data-[state=active]:bg-mainAccent text-text last:border-r-0',
+        )}
+        {...props}
+      />
+    ),
+    TabsContent: ({
+      className,
+      ...props
+    }: React.ComponentProps<typeof TabsContent>) => (
+      <TabsContent className="mt-0" {...props} />
+    ),
+    EditThisPage: ({ ...props }) => <EditThisPage {...props} />,
+    Pagination: ({ ...props }) => <Pagination {...props} />,
+    Link: ({ ...props }) => <Link {...props} />,
     ...components,
   }
 }
