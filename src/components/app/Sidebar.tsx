a2b4f0b8591a9de00156fb05257204c35d76a15e
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-12-17 01:15:28 +0100

    updated sidebar links

--- src/components/app/Sidebar.tsx
+++ src/components/app/Sidebar.tsx
@@ -3,24 +3,16 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+import { MAIN_SIDEBAR } from '@/data/sidebar-links'
+
 import { cn } from '@/lib/utils'
 
-export default function Sidebar({
-  links,
-}: {
-  links: (
-    | string
-    | {
-        href: string
-        text: string
-      }
-  )[]
-}) {
+export default function Sidebar() {
   const pathname = usePathname()
 
   return (
     <aside className="scrollbar fixed top-[88px] bg-white dark:bg-secondaryBlack h-[calc(100svh-88px)] max-h-[calc(100svh-88px)] w-[250px] overflow-y-auto border-r-4 border-border dark:border-darkNavBorder m900:hidden">
-      {links.map((item, id) => {
+      {MAIN_SIDEBAR.map((item, id) => {
         return typeof item === 'string' ? (
           <div
             key={id}
--- src/data/sidebar-links.ts
+++ src/data/sidebar-links.ts
@@ -1,18 +1,10 @@
 import { transformToSlug } from '@/lib/utils'
 
-import REACT_COMPONENTS from './components/react'
-import SHADCN_COMPONENTS from './components/shadcn'
+import COMPONENTS from './components'
 
-const REACT_LINKS = REACT_COMPONENTS.map((component) => {
+const COMPONENTS_LINKS = COMPONENTS.map((component) => {
   return {
-    href: `/react/components/${transformToSlug(component.name)}`,
-    text: component.name,
-  }
-})
-
-const SHADCN_LINKS = SHADCN_COMPONENTS.map((component) => {
-  return {
-    href: `/shadcn/components/${transformToSlug(component.name)}`,
+    href: `/components/${transformToSlug(component.name)}`,
     text: component.name,
   }
 })
@@ -21,6 +13,10 @@ const GETTING_STARTED_LINKS = [
   {
     href: '/docs',
     text: 'Introduction',
+  },
+  {
+    href: '/docs/installation',
+    text: 'Installation',
   },
   {
     href: '/docs/styling',
@@ -55,6 +51,10 @@ const MAIN_SIDEBAR = [
     text: 'Introduction',
   },
   {
+    href: '/docs/installation',
+    text: 'Installation',
+  },
+  {
     href: '/docs/styling',
     text: 'Styling',
   },
@@ -71,90 +71,19 @@ const MAIN_SIDEBAR = [
     text: 'Changelog',
   },
   'Components',
-  {
-    href: '/react/installation',
-    text: 'React',
-  },
-  {
-    href: '/shadcn/installation',
-    text: 'Shadcn',
-  },
-]
-
-const REACT_SIDEBAR = [
-  'Getting Started',
-  {
-    href: '/react/installation',
-    text: 'Installation',
-  },
-  'Components',
-  ...REACT_LINKS,
-]
-
-const SHADCN_SIDEBAR = [
-  'Getting Started',
-  {
-    href: '/shadcn/installation',
-    text: 'Installation',
-  },
-  'Components',
-  ...SHADCN_LINKS,
-]
-
-const MOBILE_REACT_SIDEBAR = [
-  'Getting started',
-  ...GETTING_STARTED_LINKS,
-  {
-    href: '/shadcn/installation',
-    text: 'Shadcn components',
-  },
-  'React',
-  {
-    href: '/react/installation',
-    text: 'Installation',
-  },
-  'React components',
-  ...REACT_LINKS,
-]
-
-const MOBILE_SHADCN_SIDEBAR = [
-  'Getting started',
-  ...GETTING_STARTED_LINKS,
-  {
-    href: '/react/installation',
-    text: 'React components',
-  },
-  'Shadcn',
-  {
-    href: '/shadcn/installation',
-    text: 'Installation',
-  },
-  'Shadcn components',
-  ...SHADCN_LINKS,
+  ...COMPONENTS_LINKS,
 ]
 
 const MOBILE_MAIN_SIDEBAR = [
   'Getting started',
   ...GETTING_STARTED_LINKS,
   'Components',
-  {
-    href: '/react/installation',
-    text: 'React',
-  },
-  {
-    href: '/shadcn/installation',
-    text: 'Shadcn',
-  },
+  ...COMPONENTS_LINKS,
 ]
 
 export {
   MAIN_SIDEBAR,
-  REACT_SIDEBAR,
-  SHADCN_SIDEBAR,
-  MOBILE_REACT_SIDEBAR,
-  MOBILE_SHADCN_SIDEBAR,
   MOBILE_MAIN_SIDEBAR,
-  REACT_LINKS,
-  SHADCN_LINKS,
+  COMPONENTS_LINKS,
   GETTING_STARTED_LINKS,
 }
