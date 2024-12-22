83ce63af0481b06ae4249e13c61e3975ee71d7d7
Samuel Breznjak <samuelbreznjak35@gmail.com>
2024-12-22 12:40:42 +0100

    added code snippets for css variables

--- src/markdown/docs/styling/Styling.tsx
+++ src/markdown/docs/styling/Styling.tsx
@@ -6,6 +6,7 @@ import colors from '@/data/colors'
 
 import Code from '@/components/app/Code'
 import { Button } from '@/components/ui/button'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 import BorderRadius from './BorderRadius'
 import BoxShadow from './BoxShadow'
@@ -66,7 +67,83 @@ export default function Styling() {
     localStorage.clear()
   }
 
-  const twConfig = `theme: {
+  const indexCss = `@tailwind base;
+@tailwind components;
+@tailwind utilities;
+  
+:root {
+  --main: ${activeColorPallete.main};
+  --overlay: rgba(0, 0, 0, 0.8); /* background color overlay for alert dialogs, modals, etc. */
+
+  --bg: ${activeColorPallete.bg};
+  --bw: #fff; /* white and secondary black e.g. utility class variant would be: bg-[#fff] dark:bg-[#212121] */
+  --blank: #000; /* blank black and white */
+  --border: #000;
+  --text: #000;
+  --mtext: #000; /* text that is placed on background with main color e.g. text color of default button */
+  --ring: #000;
+  --ring-offset: #fff;
+
+  --border-radius: ${borderRadius}px;
+  --box-shadow-x: ${boxShadowLength[0] + 'px'};
+  --box-shadow-y: ${boxShadowLength[1] + 'px'};
+  --reverse-box-shadow-x: ${-boxShadowLength[0] + 'px'};
+  --reverse-box-shadow-y: ${-boxShadowLength[1] + 'px'};
+  --base-font-weight: ${fontWeight[1]};
+  --heading-font-weight: ${fontWeight[0]};
+
+  --shadow: var(--box-shadow-x) var(--box-shadow-y) 0px 0px var(--border);
+}
+
+.dark {
+  --bg: ${activeColorPallete.darkBg};
+  --bw: #212121;
+  --blank: #fff;
+  --border: #000;
+  --text: #e6e6e6;
+  --mtext: #000;
+  --ring: #fff;
+  --ring-offset: #000;
+
+  --shadow: var(--box-shadow-x) var(--box-shadow-y) 0px 0px var(--border);
+}`
+
+  const twConfigCssVars = `theme: {
+  extend: {
+    colors: {
+      main: 'var(--main)',
+      overlay: 'var(--overlay)',
+      bg: 'var(--bg)',
+      bw: 'var(--bw)',
+      blank: 'var(--blank)',
+      text: 'var(--text)',
+      mtext: 'var(--mtext)',
+      border: 'var(--border)',
+      ring: 'var(--ring)',
+      ringOffset: 'var(--ring-offset)',
+      
+      secondaryBlack: '#212121', 
+    },
+    borderRadius: {
+      base: '${borderRadius}px'
+    },
+    boxShadow: {
+      shadow: 'var(--shadow)'
+    },
+    translate: {
+      boxShadowX: '${boxShadowLength[0] + 'px'}',
+      boxShadowY: '${boxShadowLength[1] + 'px'}',
+      reverseBoxShadowX: '${-boxShadowLength[0] + 'px'}',
+      reverseBoxShadowY: '${-boxShadowLength[1] + 'px'}',
+    },
+    fontWeight: {
+      base: '${fontWeight[1]}',
+      heading: '${fontWeight[0]}',
+    },
+  },
+},`
+
+  const twConfigUtil = `theme: {
   extend: {
     colors: {
       main: '${activeColorPallete.main}',
@@ -129,11 +206,62 @@ export default function Styling() {
       >
         Reset
       </Button>
-      <p>
-        After you choose your preferred styling, copy this to <code>theme</code>{' '}
-        section inside your tailwind config.
+      <h2>Adding styling to your project</h2>
+      <p className="mb-8">
+        You can choose either CSS variables or utility classes. Using CSS
+        variables means using single class for both light mode and dark mode
+        styling e.g. <code>text-text</code>. Using utility classes means using
+        double classes e.g.
+        <code>text-text dark:text-darkText</code>.
       </p>
-      <Code code={twConfig} name="tailwind.config.js" />
+      <Tabs defaultValue="css-vars" className="w-full">
+        <TabsList className="grid w-full grid-cols-2 overflow-x-hidden rounded-b-none m750:h-10 shadow-light rounded-t-base bg-white p-0 dark:bg-secondaryBlack">
+          <TabsTrigger
+            className="h-full border-0 border-r-2 z-10 m400:text-xs border-r-border rounded-none sm:text-base data-[state=active]:bg-main dark:data-[state=active]:text-text text-text dark:text-darkText last:border-r-0"
+            value="css-vars"
+          >
+            CSS Variables
+          </TabsTrigger>
+          <TabsTrigger
+            className="h-full border-0 border-r-2 z-10 m400:text-xs border-r-border rounded-none sm:text-base data-[state=active]:bg-main dark:data-[state=active]:text-text text-text dark:text-darkText last:border-r-0"
+            value="util"
+          >
+            Utility classes
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent className="pt-3" value="css-vars">
+          <h3>CSS Variables installation</h3>
+
+          <h4>1. Add this to index.css</h4>
+
+          <Code className="mb-6" code={indexCss} name="index.css" />
+
+          <h4>
+            2. Copy this to <code>theme</code> section inside your tailwind
+            config
+          </h4>
+
+          <Code
+            className="mb-6"
+            code={twConfigCssVars}
+            name="tailwind.config.js"
+          />
+        </TabsContent>
+        <TabsContent className="pt-3" value="util">
+          <h3>Utility classes installation</h3>
+
+          <h4>
+            1. Copy this to <code>theme</code> section inside your tailwind
+            config
+          </h4>
+
+          <Code
+            className="mb-6"
+            code={twConfigUtil}
+            name="tailwind.config.js"
+          />
+        </TabsContent>
+      </Tabs>
     </>
   )
 }
