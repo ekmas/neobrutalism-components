167c10641c17cd2e338e41ad428cac7404170187
Samuel Breznjak <samuelbreznjak35@gmail.com>
2025-03-16 22:49:41 +0100

    added variants to components.ts

--- src/data/components.ts
+++ src/data/components.ts
@@ -1,52 +1,59 @@
-import AccordionDemo from "@/components/examples/ui/accordion"
-import AlertDemo from "@/components/examples/ui/alert"
-import AlertDialogDemo from "@/components/examples/ui/alert-dialog"
-import AvatarDemo from "@/components/examples/ui/avatar"
-import BadgeDemo from "@/components/examples/ui/badge"
-import BreadcrumbDemo from "@/components/examples/ui/breadcrumb"
-import ButtonDemo from "@/components/examples/ui/button"
-import CalendarDemo from "@/components/examples/ui/calendar"
-import CardDemo from "@/components/examples/ui/card"
-import CarouselDemo from "@/components/examples/ui/carousel"
-import CheckboxDemo from "@/components/examples/ui/checkbox"
-import CollapsibleDemo from "@/components/examples/ui/collapsible"
-import ComboboxDemo from "@/components/examples/ui/combobox"
-import CommandDemo from "@/components/examples/ui/command"
-import ContextMenuDemo from "@/components/examples/ui/context-menu"
-import DataTableDemo from "@/components/examples/ui/data-table"
-import DatePickerDemo from "@/components/examples/ui/date-picker"
-import DialogDemo from "@/components/examples/ui/dialog"
-import DrawerDemo from "@/components/examples/ui/drawer"
-import DropdownMenuDemo from "@/components/examples/ui/dropdown-menu"
-import FormDemo from "@/components/examples/ui/form"
-import HoverCardDemo from "@/components/examples/ui/hover-card"
-import ImageCardDemo from "@/components/examples/ui/image-card"
-import InputDemo from "@/components/examples/ui/input"
-import InputOTPDemo from "@/components/examples/ui/input-otp"
-import LabelDemo from "@/components/examples/ui/label"
-import MarqueeDemo from "@/components/examples/ui/marquee"
-import MenubarDemo from "@/components/examples/ui/menubar"
-import NavigationMenuDemo from "@/components/examples/ui/navigation-menu"
-import PaginationDemo from "@/components/examples/ui/pagination"
-import PopoverDemo from "@/components/examples/ui/popover"
-import ProgressDemo from "@/components/examples/ui/progress"
-import RadioGroupDemo from "@/components/examples/ui/radio-group"
-import ResizableDemo from "@/components/examples/ui/resizable"
-import ScrollAreaDemo from "@/components/examples/ui/scroll-area"
-import SelectDemo from "@/components/examples/ui/select"
-import SheetDemo from "@/components/examples/ui/sheet"
-import SkeletonDemo from "@/components/examples/ui/skeleton"
-import SliderDemo from "@/components/examples/ui/slider"
-import SonnerDemo from "@/components/examples/ui/sonner"
-import SwitchDemo from "@/components/examples/ui/switch"
-import TableDemo from "@/components/examples/ui/table"
-import TabsDemo from "@/components/examples/ui/tabs"
-import TextareaDemo from "@/components/examples/ui/textarea"
-import TooltipDemo from "@/components/examples/ui/tooltip"
+import AccordionDemo from "@/examples/ui/accordion"
+import AlertDemo from "@/examples/ui/alert"
+import AlertDialogDemo from "@/examples/ui/alert-dialog"
+import AlertDestructiveDemo from "@/examples/ui/alert/destructive"
+import AvatarDemo from "@/examples/ui/avatar"
+import BadgeDemo from "@/examples/ui/badge"
+import BadgeNeutralDemo from "@/examples/ui/badge/neutral"
+import BreadcrumbDemo from "@/examples/ui/breadcrumb"
+import ButtonDemo from "@/examples/ui/button"
+import ButtonNeutralDemo from "@/examples/ui/button/neutral"
+import ButtonNoShadowDemo from "@/examples/ui/button/no-shadow"
+import ButtonReverseDemo from "@/examples/ui/button/reverse"
+import CalendarDemo from "@/examples/ui/calendar/index"
+import CalendarRangeDemo from "@/examples/ui/calendar/range"
+import CardDemo from "@/examples/ui/card"
+import CarouselDemo from "@/examples/ui/carousel"
+import CheckboxDemo from "@/examples/ui/checkbox"
+import CollapsibleDemo from "@/examples/ui/collapsible"
+import ComboboxDemo from "@/examples/ui/combobox"
+import CommandDemo from "@/examples/ui/command"
+import ContextMenuDemo from "@/examples/ui/context-menu"
+import DataTableDemo from "@/examples/ui/data-table"
+import DatePickerDemo from "@/examples/ui/date-picker"
+import DialogDemo from "@/examples/ui/dialog"
+import DrawerDemo from "@/examples/ui/drawer"
+import DropdownMenuDemo from "@/examples/ui/dropdown-menu"
+import FormDemo from "@/examples/ui/form"
+import HoverCardDemo from "@/examples/ui/hover-card"
+import ImageCardDemo from "@/examples/ui/image-card"
+import InputDemo from "@/examples/ui/input"
+import InputOTPDemo from "@/examples/ui/input-otp"
+import LabelDemo from "@/examples/ui/label"
+import MarqueeDemo from "@/examples/ui/marquee"
+import MenubarDemo from "@/examples/ui/menubar"
+import NavigationMenuDemo from "@/examples/ui/navigation-menu"
+import PaginationDemo from "@/examples/ui/pagination"
+import PopoverDemo from "@/examples/ui/popover"
+import ProgressDemo from "@/examples/ui/progress"
+import RadioGroupDemo from "@/examples/ui/radio-group"
+import ResizableDemo from "@/examples/ui/resizable"
+import ScrollAreaDemo from "@/examples/ui/scroll-area"
+import SelectDemo from "@/examples/ui/select"
+import SheetDemo from "@/examples/ui/sheet"
+import SkeletonDemo from "@/examples/ui/skeleton"
+import SliderDemo from "@/examples/ui/slider"
+import SonnerDemo from "@/examples/ui/sonner"
+import SwitchDemo from "@/examples/ui/switch"
+import TableDemo from "@/examples/ui/table"
+import TabsDemo from "@/examples/ui/tabs"
+import TextareaDemo from "@/examples/ui/textarea"
+import TooltipDemo from "@/examples/ui/tooltip"
 
 type Component = {
   name: string
   exampleComponent: React.ComponentType
+  variants?: Record<string, React.ComponentType>
   notShadcn?: boolean
 }
 
@@ -62,6 +69,10 @@ const COMPONENTS: Component[] = [
   {
     name: "Alert",
     exampleComponent: AlertDemo,
+    variants: {
+      default: AlertDemo,
+      destructive: AlertDestructiveDemo,
+    },
   },
   {
     name: "Avatar",
@@ -70,6 +81,10 @@ const COMPONENTS: Component[] = [
   {
     name: "Badge",
     exampleComponent: BadgeDemo,
+    variants: {
+      default: BadgeDemo,
+      neutral: BadgeNeutralDemo,
+    },
   },
   {
     name: "Breadcrumb",
@@ -78,10 +93,20 @@ const COMPONENTS: Component[] = [
   {
     name: "Button",
     exampleComponent: ButtonDemo,
+    variants: {
+      default: ButtonDemo,
+      reverse: ButtonReverseDemo,
+      noShadow: ButtonNoShadowDemo,
+      neutral: ButtonNeutralDemo,
+    },
   },
   {
     name: "Calendar",
     exampleComponent: CalendarDemo,
+    variants: {
+      default: CalendarDemo,
+      range: CalendarRangeDemo,
+    },
   },
   {
     name: "Card",
